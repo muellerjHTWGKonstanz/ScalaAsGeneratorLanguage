adc2904186fe33d043293ac2874433d0a7927692
Julian <muellerj@htwg-konstanz.de>
2015-09-29 10:48:32 +0200

    modified gitignore

---
+++ src/model/Shape.scala
@@ -0,0 +1,8 @@
+package model
+
+/**
+ * Created by julian on 29.09.15.
+ */
+class Shape {
+
+}
---
+++ src/util/StringToStyleParser.scala
@@ -0,0 +1,8 @@
+package util
+
+/**
+ * Created by julian on 29.09.15.
+ */
+object StringToStyleParser {
+
+}
