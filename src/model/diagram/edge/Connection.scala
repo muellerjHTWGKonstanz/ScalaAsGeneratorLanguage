0b83dded5294b61456712917626ae0a695db12b4
Julian <muellerj@htwg-konstanz.de>
2015-12-14 16:51:08 +0100

    included Connection.scala which wasnt included in last commit for some reaso...

---
+++ src/model/diagram/edge/Connection.scala
@@ -0,0 +1,11 @@
+package model.diagram.edge
+
+import model.shapecontainer.shape.geometrics.Text
+
+/**
+ * Created by julian on 11.12.15.
+ * representation of diagramm connection
+ */
+case class Connection(connection:model.shapecontainer.connection.Connection,
+                 vars:Map[String, AnyRef] = Map(),/*TODO String is a Mockup for EcoreAttribute*/
+                 vals:Map[String, AnyRef] = Map())
